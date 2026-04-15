c6eef241b13eb0432cd9179942239c1dc85e0e4c
Bruno <bruno@panop.local>
2026-04-15 18:05:09 -0300

    style: deploy machine-learning saliency mask to completely drop the gray background artifact

--- clean_logo.py
+++ clean_logo.py
@@ -1,32 +1,20 @@
+from rembg import remove
 from PIL import Image
 
-img = Image.open(r'C:\Users\bruno\.gemini\antigravity\brain\b2e86185-a271-4eb9-8ed9-ae9bab958c45\panop_logo_red_1776285238593.png').convert("RGBA")
-datas = img.getdata()
-new_data = []
-for item in datas:
-    m = max(item[0], item[1], item[2])
-    if m < 20: 
-        a = 0
-    elif m > 80:
-        a = 255
-    else:
-        a = int((m - 20) * (255.0 / 60.0))
-        
-    if a > 0 and a < 255 and m > 0:
-        new_data.append((min(255, int(item[0]*255/m)), min(255, int(item[1]*255/m)), min(255, int(item[2]*255/m)), a))
-    else:
-        new_data.append((item[0], item[1], item[2], a))
+input_path = r'C:\Users\bruno\.gemini\antigravity\brain\b2e86185-a271-4eb9-8ed9-ae9bab958c45\panop_logo_red_1776285238593.png'
+output_path = 'assets/logo.png'
 
-img.putdata(new_data)
-bbox = img.getbbox()
+inp = Image.open(input_path)
+out = remove(inp)
+
+bbox = out.getbbox()
 if bbox:
-    img = img.crop(bbox)
+    out = out.crop(bbox)
+out.save(output_path)
 
-img.save('assets/logo.png')
-
-w, h = img.size
+w, h = out.size
 sq_size = max(w, h)
 sq_img = Image.new('RGBA', (sq_size, sq_size), (0,0,0,0))
-sq_img.paste(img, ((sq_size - w) // 2, (sq_size - h) // 2))
+sq_img.paste(out, ((sq_size - w) // 2, (sq_size - h) // 2))
 sq_img = sq_img.resize((256, 256), Image.Resampling.LANCZOS)
 sq_img.save('assets/panop.ico', format='ICO')
